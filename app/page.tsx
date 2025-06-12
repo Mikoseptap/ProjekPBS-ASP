208a44b546956e74168a8012cde2d41ef46bfb6c
RastomiOF <raspamungkas07@gmail.com>
2025-06-12 09:07:32 +0700

    rastomi: Mengubah nama/judul tempat makan dengan yang sesuai

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="bg-[#db641f4] min-h-screen font-sans">
       {/* Header */}
       <header className="bg-[#db641f] text-white px-6 py-4 flex justify-between items-center">
-        <h1 className="text-xl font-bold">Food Delivery</h1>
+        <h1 className="text-xl font-bold">Waroeng MakJum</h1>
         <nav className="space-x-4">
           <a href="#" className="hover:underline">Home</a>
           <a href="#" className="hover:underline">About</a>
