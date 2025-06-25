0846c5775b1179c12f9e24c107741c5e041c6427
Mikoseptap <Mikosepta22@gmail.com>
2025-06-25 20:42:01 +0700

    Mikoseptap: Memasukan CORS pada product/routes.ts

--- backend/app/api/product/route.ts
+++ backend/app/api/product/route.ts
@@ -61,5 +61,15 @@ export const POST = async (req: NextRequest) => {
   );
 };
 
+export const OPTIONS = async () => {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+};
 
 
